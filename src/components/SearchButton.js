c4b37c229772711a2329af6a57aabae061845dc7
jwjb1020 <jwjb1020@gmail.com>
2023-08-08 02:01:30 +0900

    2023.08.08

--- src/components/SearchButton.js
+++ src/components/SearchButton.js
@@ -2,18 +2,17 @@ import React from 'react'
 
 export default function SearchButton() {
   return (
-    <div className='flex-auto'>
-          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+   
+          <div className="flex-auto mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form className="space-y-6" action="#" method="POST">
             <div>
               <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                Email address
+                location
               </label>
               <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
+                  id="location"
+                  name="location"
+                  type="text"
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
@@ -44,6 +43,6 @@ export default function SearchButton() {
               </div>
               </form>
               </div>
-    </div>
+   
   )
 }
